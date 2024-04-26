95fec6c5ac40d1519770121cfb56d295f792ab83
corenting <corenting@theodo.fr>
2024-04-26 10:57:40 +0200

    Add test on authentification

--- backend/tests/Service/TuneControllerTest.php
+++ backend/tests/Service/TuneControllerTest.php
@@ -3,15 +3,51 @@
 namespace App\Tests\Service;
 
 use App\Tests\Utils\FixtureAwareCaseTrait;
+use http\Client;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\BrowserKit\AbstractBrowser;
 use Symfony\Component\HttpFoundation\Response;
 
 class TuneControllerTest extends WebTestCase
 {
     use FixtureAwareCaseTrait;
-    public function testAssertTrue():void {
+    public function testAssertTrue(): void
+    {
         $this->assertTrue(true);
     }
+    //protected function createAuthenticatedClient($email = 'corenting@theodo.fr', $password = 'theodo'): AbstractBrowser
+    protected function createAuthenticatedClient($email = 'test@test', $password = 'TestPassword'): AbstractBrowser
+    {
+        $client = static::createClient();
+        $client->request(
+            'POST',
+            '/api/login_check',
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+            json_encode([
+                'email' => $email,
+                'password' => $password,
+            ])
+        );
+//        fwrite(STDERR, print_r($client->getResponse()->getContent(), TRUE));
+
+        $data = json_decode($client->getResponse()->getContent(), true);
+
+        $client->setServerParameter('HTTP_Authorization', sprintf('Bearer %s', $data['token']));
+
+        return $client;
+    }
+    public function testGetPages()
+    {
+        $this->addToAssertionCount(1);
+        echo "debut test";
+        $client = $this->createAuthenticatedClient();
+        $client->request('GET', '/api/tune');
+        $this->assertResponseIsSuccessful();
+    }
+
+
     public function testIfResponseIsSuccessful(): void
     {
         $client = static::createClient();
@@ -20,11 +56,13 @@ class TuneControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/api/tune');
         $this->assertResponseIsSuccessful();
     }
+
     public function testGetTune(): void
     {
         $client = static::createClient();
 
         FixtureAwareCaseTrait::loadFixturesWithContainer(static::getContainer(), "tunes");
+        FixtureAwareCaseTrait::loadFixturesWithContainer(static::getContainer(), "users");
 
         $client->request('GET', '/api/tune?filter=Thriller');
         $response = $client->getResponse();
